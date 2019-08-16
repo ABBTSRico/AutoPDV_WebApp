f2248b3f554d4e638ea621719442f5df92b7debd
Manuel <habi86@bluewin.ch>
2019-08-16 11:54:18 +0200

    Add PopUp edit/append

--- Features/PopUp/popUp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    function openForm() {
-        document.getElementById("myForm").removeClass("form-popup-hidden");
-      }
-      
-      function closeForm() {
-        document.getElementById("myForm").removeClass("form-popup-hidden");
-      }
-})
